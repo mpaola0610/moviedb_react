2b83e5b9ac96c0fd1460dca16da9e593ac143a12
mcrina <mcrinacervantes@gmail.com>
2021-10-14 19:48:48 -0500

    3rd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //components
 import Header from './components/Header';
+import Home from './components/Home';
 
 
 //styles
@@ -10,7 +11,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      Start here.
+      <Home/>
+      
       <GlobalStyle />
     </div>
   );
---
+++ src/components/HeroImage/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+//styles
+import {Wrapper, Content, Text } from './HeroImage.styles';
+
+const HeroImage = ()
